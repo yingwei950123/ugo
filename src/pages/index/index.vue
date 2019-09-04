6fffeef87153ec7a8dc520619772d5000d0da58e
yingwei950123 <15210443708@163.com>
2019-09-04 13:58:16 +0800

    首页静态

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,95 +1,147 @@
 <template>
-  <div></div>
+  <div>
+    <div class="search">
+      <div class="search-box">
+        <input placeholder="搜索">
+      </div>
+    </div>
+    <swiper class="banner" indicator-dots indicator-color="rgba(255, 255, 255, 0.6)" indicator-active-color="#fff">
+      <swiper-item>
+        <image src="/static/uploads/banner1.png" />
+      </swiper-item>
+      <swiper-item>
+        <image src="/static/uploads/banner2.png" />
+      </swiper-item>
+      <swiper-item>
+        <image src="/static/uploads/banner3.png" />
+      </swiper-item>
+    </swiper>
+    <div class="navs">
+      <navigator>
+        <image src="/static/uploads/icon_index_nav_1@2x.png" />
+      </navigator>
+      <navigator>
+        <image src="/static/uploads/icon_index_nav_2@2x.png" />
+      </navigator>
+      <navigator>
+        <image src="/static/uploads/icon_index_nav_3@2x.png" />
+      </navigator>
+      <navigator>
+        <image src="/static/uploads/icon_index_nav_4@2x.png" />
+      </navigator>
+    </div>
+    <div class="floor">
+      <div class="f-title">
+        <image src="/static/uploads/pic_floor01_title.png" />
+      </div>
+      <div class="f-box">
+        <div>
+          <image src="/static/uploads/pic_floor01_1@2x.png" />
+        </div>
+        <div>
+          <image src="/static/uploads/pic_floor01_2@2x.png" />
+          <image src="/static/uploads/pic_floor01_3@2x.png" />
+          
+        </div>
+        <div>
+          <image src="/static/uploads/pic_floor01_4@2x.png" />
+          <image src="/static/uploads/pic_floor01_5@2x.png" />
+        </div>
+      </div>
+    </div>
+    <div class="floor">
+      <div class="f-title">
+        <image src="/static/uploads/pic_floor02_title.png" />
+      </div>
+      <div class="f-box">
+        <div>
+          <image src="/static/uploads/pic_floor02_1@2x.png" />
+        </div>
+        <div>
+          <image src="/static/uploads/pic_floor02_2@2x.png" />
+          <image src="/static/uploads/pic_floor02_3@2x.png" />
+          
+        </div>
+        <div>
+          <image src="/static/uploads/pic_floor02_4@2x.png" />
+          <image src="/static/uploads/pic_floor02_5@2x.png" />
+        </div>
+      </div>
+    </div>
+    <div class="floor">
+      <div class="f-title">
+        <image src="/static/uploads/pic_floor03_title.png" />
+      </div>
+      <div class="f-box">
+        <div>
+          <image src="/static/uploads/pic_floor03_1@2x.png" />
+        </div>
+        <div>
+          <image src="/static/uploads/pic_floor03_2@2x.png" />
+          <image src="/static/uploads/pic_floor03_3@2x.png" />
+          
+        </div>
+        <div>
+          <image src="/static/uploads/pic_floor03_4@2x.png" />
+          <image src="/static/uploads/pic_floor03_5@2x.png" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import card from '@/components/card'
-
 export default {
-  data () {
-    return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
-    }
-  },
-
-  components: {
-    card
-  },
-
-  methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
-  },
-
-  created () {
-    // let app = getApp()
-  }
 }
 </script>
 
 <style scoped>
-.userinfo {
+.search-box {
+  padding: 16rpx 30rpx;
+  background-color: #ea4451;
+
+}
+.search-box input {
+  background-color: #fff;
+  text-align: center;
+  font-size: 30rpx;
+}
+.banner {
+  width: 750rpx;
+  height: 320rpx;
+}
+.banner image {
+  width: 750rpx;
+  height: 320rpx;
+}
+.navs {
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  padding: 20rpx 0;
+  justify-content: space-around;
 }
-
-.userinfo-avatar {
+.navs image {
   width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
+  height: 140rpx;
 }
-
-.userinfo-nickname {
-  color: #aaa;
+.f-title image{
+  width: 750rpx;
+  height: 60rpx;
 }
-
-.usermotto {
-  margin-top: 150px;
+.f-box {
+  overflow: hidden;
 }
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
+.f-box div{
+  float: left;
 }
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
+.f-box div:nth-child(1) image{
+  width: 223rpx;
+  height: 386rpx;
+  margin-right: 10rpx;
+  margin-left: 20rpx;
 }
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
+.f-box div:nth-child(2) image,.f-box div:nth-child(3) image{
+  width: 223rpx;
+  height: 188rpx;
+  margin-right: 10rpx;
 }
 </style>
